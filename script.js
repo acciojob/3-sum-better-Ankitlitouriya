43f1349db7b4d42e5ad8eaf39c2d2ac60f933993
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-08-22 05:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function threeSum(nums, target) {
+  // Sort the array
+  nums.sort((a, b) => a - b);
 
+  let closestSum = nums[0] + nums[1] + nums[2];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const currentSum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
+}
+console.log(threeSum([-1,2,1,-4], 1))
 module.exports = threeSum;
